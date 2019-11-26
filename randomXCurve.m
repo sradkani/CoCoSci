5496b738e6c6ea5a5ea15deced44e61ec355f6d2
setayesh <radkani@mit.edu>
2019-11-26 10:51:01 -0500

    give alphabet as input

--- randomXCurve.m
+++ randomXCurve.m
@@ -1,4 +1,4 @@
-function curve = randomXCurve(sizeAlphabet, maxMotifLength, sequence, delta, alpha)
+function curve = randomXCurve(alphabet, maxMotifLength, sequence, delta, alpha)
 % takes as input the sequence and parameters for the inferring HMM and 
 % returns a curve of random(X) for each point in the  sequence 
 % (starting from the 2nd state)
@@ -8,5 +8,5 @@ curve = nan(1, length(sequence)-1);
 
 % compute random(X) at each point in the sequence
 for i = 2:length(sequence)
-    curve(i-1) = findRandomness(sizeAlphabet, maxMotifLength, sequence(1:i), delta, alpha);
+    curve(i-1) = findRandomness(alphabet, maxMotifLength, sequence(1:i), delta, alpha);
 end
