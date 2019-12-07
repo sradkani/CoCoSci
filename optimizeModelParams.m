67ffd2f4ba5e8e811f5481703c30a43e01b82f7b
setayesh <radkani@mit.edu>
2019-12-07 17:54:56 -0500

    small correction

--- optimizeModelParams.m
+++ optimizeModelParams.m
@@ -3,9 +3,10 @@ function [modelParams, optCorr] = optimizeModelParams(sequences, subjRand_human,
     modelParams.alphabet = alphabet;
     modelParams.maxMotifLength = maxMotifLength;
     
-    fun = @(params) findModelHumanCorr(sequences, subjRand_human, modelParams, params(1), params(2));
+    fun = @(params) -1 * findModelHumanCorr(sequences, subjRand_human, modelParams, params(1), params(2));
     [optParams, optCorr] = fminsearch(fun, [delta0, alpha0]);
     
+    optCorr = -optCorr;
     modelParams.delta = optParams(1);
     modelParams.alpha = optParams(2);
 end
