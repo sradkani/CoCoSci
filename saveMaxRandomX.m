6ccafa7e52e48ae186a041b1d7b609ec9458ee4e
galraz@gmail.com <galraz@gmail.com>
2019-12-08 19:43:30 -0500

    generates .mat file with maxrandom x for given parameters

---
+++ saveMaxRandomX.m
@@ -0,0 +1,12 @@
+function saveMaxRandomX(alphabet, maxMotifLength, maxSeqLength, delta, alpha)
+% this creates a mat file with maxRandom(x) for a specified alpha and delta
+% from length 2 to maxSeqLength -1
+
+maxRandomX = nan(1, maxSeqLength-1);
+
+% normalize by maxRandomX
+for i = 2:30
+    maxRandomX(i-1) = findMaxRandomX(alphabet, i, maxMotifLength, delta, alpha);
+end
+
+save(sprintf('maxRandomX_delta%.2f_alpha%.2f.mat', delta, alpha), 'maxRandomX')
