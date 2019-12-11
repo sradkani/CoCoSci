e3480a56a41834d73ad2476bafc2905212e18e2b
galraz@gmail.com <galraz@gmail.com>
2019-12-10 19:03:55 -0500

    changes

--- reportPlots.R
+++ reportPlots.R
@@ -2,6 +2,22 @@ library(tidyverse)
 
 
 setwd("/Users/galraz1/Developer/CoCoSci")
-read_csv('Rdata.csv')
+df1 <- data.frame(read_csv('Rdata.csv'))
 
-ggplot2()
+
+# lineplot randomXbins vs disengaged
+ggplot(df1, aes(binMeans, propDisengaged)) + geom_line()+ geom_point()
+
+
+df2 <- data.frame(read_csv('Rdata2.csv'))
+
+# dotplot for logistic regression
+ggplot(df2, aes(diffs, disengaged))  + geom_point() + theme_classic()
+
+# boxplot change in randomX for disengaged vs engaged 
+ggplot(df2, aes(x=as.factor(disengaged), y=diffs)) + geom_boxplot()
+
+
+curvedf <- data.frame(read_csv('Rdata2.csv'))
+
+
